80cf476629ecf12cd6ea4cf0166ca10f5859b460
Lutz Kuhlmann <LutzKuhlmann92@gmail.com>
2025-06-24 01:21:38 +0200

    init

---
+++ src/test/java/com/lingosphinx/quiz/TestQuizApplication.java
@@ -0,0 +1,11 @@
+package com.lingosphinx.quiz;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(QuizApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
---
+++ src/test/java/com/lingosphinx/quiz/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.lingosphinx.quiz;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
