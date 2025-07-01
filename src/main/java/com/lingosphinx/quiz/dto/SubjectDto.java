d72c57d59fd8b8296b9afa1b0e9125270220b405
Lutz Kuhlmann <LutzKuhlmann92@gmail.com>
2025-07-01 16:39:13 +0200

    removed JPA annotations from dtos

--- src/main/java/com/lingosphinx/quiz/dto/SubjectDto.java
+++ src/main/java/com/lingosphinx/quiz/dto/SubjectDto.java
@@ -4,18 +4,10 @@ import com.lingosphinx.quiz.domain.LanguageCode;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Table(name = "subject")
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor @Builder
 public class SubjectDto {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
-
-    @Column(nullable = false)
     private String name;
-
-    @Column(nullable = false)
     private LanguageCode language;
 }
